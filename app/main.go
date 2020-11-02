ab96ea6a0aaa5befeaf6d8a557d71e602dae1a14
Mikhail Kokotov <mikhail.kokotov@roonyx.tech>
2020-11-02 14:20:13 +0500

    add save edit delete posts

--- app/main.go
+++ app/main.go
@@ -5,7 +5,11 @@ import (
 	"log"
 	"net/http"
 	"html/template"
+
+	"./models"
 )
+
+var posts map[string]*models.Post
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
@@ -13,7 +17,9 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, err.Error())
 
 	}
-	t.ExecuteTemplate(w, "index", nil)
+	fmt.Println(posts)
+
+	t.ExecuteTemplate(w, "index", posts)
 }
 
 func writeHandler(w http.ResponseWriter, r *http.Request) {
@@ -24,10 +30,62 @@ func writeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	t.ExecuteTemplate(w, "write", nil)
 }
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+
+	}
+
+	id := r.FormValue("id")
+	post, found := posts[id]
+	if !found {
+		http.NotFound(w, r)
+	}
+
+	t.ExecuteTemplate(w, "write", post)
+}
+
+func savePostHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("id")
+	title := r.FormValue("title")
+	content := r.FormValue("content")
+
+	var post *models.Post
+	if id != "" {
+		post = posts[id]
+		post.Title = title
+		post.Content = content
+	} else {
+		id = GenerateId()
+		post := models.NewPost(id, title, content)
+		posts[post.Id] = post
+	}
+
+	http.Redirect(w, r, "/", 302)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("id")
+	if id == "" {
+		http.NotFound(w, r)
+	}
+
+	delete(posts,id)
+
+	http.Redirect(w, r, "/", 302)
+}
+
 func main() {
+	posts = make(map[string]*models.Post, 0)
+
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/write", writeHandler)
+	http.HandleFunc("/SavePost", savePostHandler)
+	http.HandleFunc("/edit", editHandler)
+	http.HandleFunc("/delete", deleteHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
